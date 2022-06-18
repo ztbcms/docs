21f22c3c94f6be47f485c92ec2a709170d51c673
Jayin <tonjayin@gmail.com>
2022-06-18 12:37:36 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,17 +17,16 @@ module.exports = {
   themeConfig: {
     // Git 仓库和编辑链接
     repo: 'https://github.com/ztbcms/ztbcms/tree/v3',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为 "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: '查看源码',
     // 以下为可选的编辑链接选项
-    // 假如你的文档仓库和项目本身不在一个仓库：
+    // 文档仓库
     docsRepo: 'https://github.com/ztbcms/docs',
-    // 假如文档不是放在仓库的根目录下：
+    // 文档根目录
     docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
+    // 文档分支
     docsBranch: 'develop',
-    // 默认是 false, 设置为 true 来启用
+    // 是否开启编辑链接
     editLinks: true,
     // 默认为 "Edit this page"
     editLinkText: '编辑此页',
@@ -46,10 +45,6 @@ module.exports = {
     // 最后更新时间
     lastUpdated: '更新于', // string | boolean
     // 侧栏
-    // sidebar: [
-    //   '/',
-    //   ['http://baidu.com', '模块']
-    // ],
     sidebar: 'auto',
     sidebarDepth: 2,
   },
